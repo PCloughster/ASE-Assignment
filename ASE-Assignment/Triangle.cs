fd7dae791b05f2a6e7da65d3d4fbe5afcef60914
pcloughster <patrick@sibbit.co.uk>
2023-11-16 03:03:52 +0000

    triangle.cs XML documentation

--- ASE-Assignment/Triangle.cs
+++ ASE-Assignment/Triangle.cs
@@ -7,22 +7,51 @@ using System.Threading.Tasks;
 
 namespace ase_assignment
 {
+    /// <summary>
+    /// Class for the Triangle object inherits from Shape.cs
+    /// </summary>
     public class Triangle : Shape
     {
         int side1;
         int side2;
         string shape = "triangle";
+        /// <summary>
+        /// constructor for a right-angled triangle taking two side measurements
+        /// </summary>
+        /// <param name="colour">colour of triangle</param>
+        /// <param name="fillType">is triangle filled</param>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y coordinate</param>
+        /// <param name="side1">side1 length</param>
+        /// <param name="side2">side2 length</param>
         public Triangle(Color colour, Boolean fillType, int x, int y, int side1, int side2) : base(colour, fillType, x, y)
         {
             this.side1 = side1;
             this.side2 = side2;
         }
+        /// <summary>
+        /// constructor for a right-angled triangle taking one side measurements
+        /// 
+        /// side2 length is set to side1 length to keep the rest of the class the same
+        /// </summary>
+        /// <param name="colour">colour of triangle</param>
+        /// <param name="fillType">is triangle filled</param>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y coordinate</param>
+        /// <param name="side1">side1 length</param>
         public Triangle(Color colour, Boolean fillType, int x, int y, int side1) : base(colour, fillType, x, y)
         {
             this.side1 = side1;
             this.side2 = side1;
         }
-
+        /// <summary>
+        /// generates the vertices of the triangle from the provided coordinates and sides
+        /// </summary>
+        /// <param name="x">X Coordinate</param>
+        /// <param name="y">Y Coordinate</param>
+        /// <param name="side1">added to X coordinate to generate second vertex</param>
+        /// <param name="side2">added to Y coordinate to generate third vertex</param>
+        /// <returns>vertices of the triangle</returns>
         private Point[] SetPoints(int x, int y, int side1, int side2)
         {
             Point point1 = new Point(x, y);
@@ -34,6 +63,10 @@ namespace ase_assignment
             };
             return points;
         }
+        /// <summary>
+        /// Draws the triangle on the specified graphics object
+        /// </summary>
+        /// <param name="g">graphics object to draw triangle on</param>
         public override void Draw(Graphics g)
         {
             Point[] points = SetPoints(x, y, side1, side2);
@@ -48,7 +81,10 @@ namespace ase_assignment
                 g.DrawPolygon(p, points);
             }
         }
-
+        /// <summary>
+        /// ToString override to include specific triangle variables
+        /// </summary>
+        /// <returns>string of object information</returns>
         public override string ToString()
         {
             return shape+": "+base.ToString() + "  " + this.side1 + this.side2;
