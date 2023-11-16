bfb2bf658c695c1e2a060fcfe071e48569e59155
pcloughster <patrick@sibbit.co.uk>
2023-11-16 03:07:32 +0000

    XML documentation for Rectangle.cs

--- ASE-Assignment/Rectangle.cs
+++ ASE-Assignment/Rectangle.cs
@@ -6,21 +6,44 @@ using System.Threading.Tasks;
 
 namespace ase_assignment
 {
+    /// <summary>
+    /// Class for the Rectangle object inherits from Shape.cs
+    /// </summary>
     public class Rectangle:Shape
     {
         int width, height;
         string shape = "rectangle";
+        /// <summary>
+        /// constructor for a rectangle taking height and width measurements
+        /// </summary>
+        /// <param name="colour">colour of rectangle</param>
+        /// <param name="fillType">is triangle filled</param>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y coordinate</param>
+        /// <param name="width">width of rectangle</param>
+        /// <param name="height">height of rectangle</param>
         public Rectangle(Color colour, Boolean fillType, int x, int y, int width, int height) : base(colour, fillType, x, y)
         {
             this.width = width;
             this.height = height;
         }
+        /// <summary>
+        /// constructor for a rectangle taking only the width measurement to draw a square, height is set to be equal to the width
+        /// </summary>
+        /// <param name="colour">colour of rectangle</param>
+        /// <param name="fillType">is triangle filled</param>
+        /// <param name="x">X coordinate</param>
+        /// <param name="y">Y coordinate</param>
+        /// <param name="width">width of rectangle</param>
         public Rectangle(Color colour, Boolean fillType, int x, int y, int width) : base(colour, fillType, x, y)
         {
             this.width = width;
             this.height = width;
         }
-
+        /// <summary>
+        /// Draws the rectangle on the specified graphics object
+        /// </summary>
+        /// <param name="g">graphics object to draw rectangle on</param>
         public override void Draw(Graphics g)
         {
             if (fillType == true)
@@ -34,7 +57,10 @@ namespace ase_assignment
                 g.DrawRectangle(p, x, y, width, height);
             }
         }
-
+        /// <summary>
+        /// ToString override to include specific rectangle variables
+        /// </summary>
+        /// <returns>string of object information</returns>
         public override string ToString()
         {
             return shape+": "+base.ToString()+"  "+this.width+","+this.height;
